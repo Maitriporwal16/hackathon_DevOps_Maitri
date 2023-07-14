1321ae56cefb2067e411047af01495b02db95075
Maitri Porwal <63542734+Maitriporwal16@users.noreply.github.com>
2023-07-14 23:01:10 +0530

    Just testing revert command

---
+++ prowler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install packages
+sudo yum update -y
+sudo yum install python3 -y
+sudo yum install screen -y
+sudo yum install zip -y
+
+# env vars
+account=$(aws sts get-caller-identity | jq --raw-output '.Account') 
+mkdir ${account}-results
+
+# Prowler
+cd ~
+git clone https://github.com/prowler-cloud/prowler
+pip3 install detect-secrets --user
+cd prowler
+screen -dmS prowler sh -c "./prowler -M csv,html;cd~; zip -r ${account}-results/prowler-${accounty}.zip /home/cloudshell-user/prowler/output"
+
+# Check on screen sessions
+screen -ls
